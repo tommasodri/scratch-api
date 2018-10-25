680437c6fdd1be8485a9615f196bde2fb3949a3e
Tommaso Dri <tommaso.dri@gmail.com>
2018-10-25 12:52:09 +0200

    Fixing the mistake

--- create.js
+++ create.js
@@ -4,7 +4,6 @@ import { success, failure } from "./libs/response-lib";
 
 export async function main(event, context, callback) {
 
-  gibberish.what;
   const data = JSON.parse(event.body);
   const params = {
     TableName: process.env.tableName,
